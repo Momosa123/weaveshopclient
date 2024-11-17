eba31a6d793433f67593244a8083b0bac80a2aa3
Mouhamadou Sarr <mouhamadoul.sarr@gmail.com>
2024-11-17 11:39:13 +0100

    conditionally render Home products or search results

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import SearchResults from "./components/searchResults";
-import AllProducts from "./components/allProducts";
+import AllProducts from "./components/homeProducts";
 export default async function Home(props: {searchParams?: Promise<{search: string}>}) {
   const searchParams = await props.searchParams || {search: ""};
-  const search =  searchParams.search;
+  const search = searchParams.search;
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <AllProducts />
-       {/*  <SearchResults query={search} /> */}
+        {search ? (
+          <SearchResults query={search} />
+        ) : (
+          <AllProducts />
+        )}
       </main>
-    
     </div>
   );
 }
