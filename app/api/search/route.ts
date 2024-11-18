cd9390511dcc3a13c6112aac8a30ebbbe55cc4e9
Mouhamadou Sarr <mouhamadoul.sarr@gmail.com>
2024-11-18 14:28:49 +0100

    using search functions in route

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,4 +1,4 @@
-import { searchProductsByImage, searchProductsByText } from '@/utils/searchProducts';
+import { searchProductsByImage, searchProductsByText } from '@/lib/searchProducts';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
