468731345bf23cd2bd35e4e65daba0b9394e74d8
Mouhamadou Sarr <mouhamadoul.sarr@gmail.com>
2024-11-18 16:15:54 +0100

    product details page

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,23 @@
+import ProductDetail from "@/app/components/productDetail";
+import { getProduct } from "@/lib/getProducts";
+
+export default async function ProductPage({ params }: { params: { id: string } }) {
+    const response = await getProduct(params.id);
+    console.log(response)
+    const product = response.data.Get.FashionProducts[0];
+
+    if (!product) {
+        return <div>Product not found</div>;
+    }
+
+    return (
+        <ProductDetail 
+            main_image={product.main_image}
+            title={product.title}
+            average_rating={product.average_rating}
+            price={product.price}
+            
+            reviews_count={product.reviews_count}
+        />
+    );
+}
