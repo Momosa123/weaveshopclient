8007dc1ff0c40c456bfecf4e2cb10e2d5b409ff1
Mouhamadou Sarr <mouhamadoul.sarr@gmail.com>
2024-11-20 10:08:12 +0100

    addition of a container to make the footer stay at the bottom

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,14 +4,19 @@ import SearchBar from "../components/searchBar";
 import SearchResults from "../components/searchResults";
 import { useState } from "react";
 import { ProductType } from "../definition";
+import Container from "../components/Container";
 
 export default function SearchPage() {
     const [results, setResults] = useState<WeaviateReturn<ProductType> | undefined>(undefined); 
 
     return (
-        <div className="w-full max-w-3xl mx-auto p-4">
-            <SearchBar setResults={setResults} isMobile />  
+        <Container>
+             <div className="flex flex-col gap-4 mb-6 mt-8 max-w-3xl mx-auto">
+                <h1 className="text-2xl font-bold text-center">Find your perfect look using AI search powered by Weaviate</h1>
+                <SearchBar setResults={setResults} isMobile /> 
+            </div>
             <SearchResults results={results}  />
-        </div>
+
+        </Container>
     );
 }
