64f70aab6c57c8ef1169ef5fc56d8cbe60f08174
Mouhamadou Sarr <mouhamadoul.sarr@gmail.com>
2024-11-20 20:17:11 +0100

    changed type definition of search result

--- app/definition.ts
+++ app/definition.ts
@@ -1,20 +1,21 @@
-export interface ProductType {
-    metadata:Record<string, unknown>;
-    uuid:string;
-    vectors:Record<string, unknown>;
-    [key: string]: any;
-    properties: {
-    title: string;
-    average_rating: number;
-    rating_number: number;
-    price: number;
-    main_image: string;
-    }
-}
+import { Properties } from "weaviate-client";
+
+  
 export interface ProductGraphQL {
     _additional:{id:string};
     main_image: string;
     title: string;
     average_rating: number;
     price: number;
+}   
+
+export interface ProductProperties extends Properties{
+    uuid: string;
+    properties: {
+        title: string;
+        average_rating: number;
+        rating_number: number;
+        price: number;
+        main_image: string;
+      };
 }
